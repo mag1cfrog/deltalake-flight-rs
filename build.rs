2e8267b7f98a01abbd5d75f037a1202d414d9205
hanbow <harrywong2017@gmail.com>
2025-02-24 13:29:40 -0700

    add dependencies for prost and tonic, update DeltaFlight service method signatures, and create build script for protobuf compilation

---
+++ build.rs
@@ -0,0 +1,23 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // 1. Configure protoc to find external dependencies
+    let proto_dir = "proto";
+    // let arrow_flight_proto = "proto/arrow/flight/protocol/flight.proto";
+    
+    // 2. Configure prost
+    let mut prost_config = prost_build::Config::new();
+
+    prost_config.bytes(["."]); // Use Bytes instead of Vec<u8>
+    prost_config.type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]");
+
+    // 3. Generate Tonic server code
+    tonic_build::configure()
+        .build_server(true) // Generate server code
+        .build_client(true) // Generate client code
+        .compile_protos_with_config(
+            prost_config,
+            &["proto/deltaflight.proto"], // Your protos
+            &[proto_dir], // Include paths
+        )?;
+
+    Ok(())
+}
